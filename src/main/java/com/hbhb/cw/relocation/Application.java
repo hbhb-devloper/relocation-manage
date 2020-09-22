58e0bb93c62b8beceb75c5591839933186917035
xiaokang <13148807065@163.com>
2020-09-23 04:31:56 +0800

    恢复

--- src/main/java/com/hbhb/cw/relocation/Application.java
+++ src/main/java/com/hbhb/cw/relocation/Application.java
@@ -4,12 +4,14 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author dxk
  */
+@RefreshScope
 @EnableScheduling
 @EnableSwagger2Doc
 @EnableFeignClients
