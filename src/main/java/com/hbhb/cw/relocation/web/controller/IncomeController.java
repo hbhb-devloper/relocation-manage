dca658073483161eeb08e0434aa1d474285c57ef
huangyukun <76265497@qq.com>
2020-10-20 17:48:12 +0800

    修改接口和swagger注释 [ci skip]

--- src/main/java/com/hbhb/cw/relocation/web/controller/IncomeController.java
+++ src/main/java/com/hbhb/cw/relocation/web/controller/IncomeController.java
@@ -5,7 +5,10 @@ import com.hbhb.cw.relocation.web.vo.IncomeResVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.beetl.sql.core.page.PageResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author xiaokang
@@ -29,7 +29,7 @@ public class IncomeController {
 
     @Operation(summary = "迁改收款查询台账列表")
     @GetMapping("/list")
-    public Page<IncomeResVO> getIncomeList(
+    public PageResult<IncomeResVO> getIncomeList(
         @Parameter(description = "页码，默认为1") @RequestParam(required = false) Integer pageNum,
         @Parameter(description = "每页数量，默认为10") @RequestParam(required = false) Integer pageSize,
         @Parameter(description = "接收参数实体") IncomeReqVO cond) {
